bab7039c74128a4bf116d46ce3c4af18b9de0a42
Aleksandr Tochilin <alex8079@gmail.com>
2023-07-13 03:22:13 +0300

    update config

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,44 +4,44 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
-  devtool: "source-map",
-  devServer: {
-    historyApiFallback: true,
-    static: path.resolve(__dirname, './'),
-    open: true,
-    compress: true,
-    hot: true,
-    port: 3000,
-  },
-  entry: {
-    main: path.resolve(__dirname, './src/index.js')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
-      template: path.resolve(__dirname, './src/index.html'), // template file
-      filename: 'index.html', // output file
-    }),
-	new CleanWebpackPlugin(),
-	new webpack.HotModuleReplacementPlugin(),
-  ],
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
-  },
-  module: {
-    rules: [
-		{
-		 test: /\.(scss|css)$/,
-		 use: ['style-loader', 'css-loader', 'sass-loader'],
-		},
-      // JavaScript
-      {
-       test: /\.js$/,
-       exclude: /node_modules/,
-       use: ['babel-loader'],
-      },
+    mode: 'development',
+    devtool: "source-map",
+    devServer: {
+        historyApiFallback: true,
+        static: path.resolve(__dirname, './'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 3000,
+    },
+    entry: {
+        main: path.resolve(__dirname, './src/index.js')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/index.html'), // template file
+            filename: 'index.html', // output file
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
     ],
-  },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,54 +1,56 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: {
-    main: path.resolve(__dirname, './src/index.js')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
-      template: path.resolve(__dirname, './src/index.html'), // template file
-      filename: 'index.html', // output file
-    }),
-	new CleanWebpackPlugin(),
-	new MiniCssExtractPlugin()
-  ],
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
-  },
-  module: {
-    rules: [
-		{
-		 test: /\.(scss|css)$/,
-		 use: [
-			MiniCssExtractPlugin.loader,
-			'css-loader',
-			'postcss-loader',
-			'sass-loader'
-		 ],
-		},
-		{
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-		},
-      // JavaScript
-      {
-       test: /\.js$/,
-       exclude: /node_modules/,
-       use: ['babel-loader'],
-      },
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/index.html'), // template file
+            filename: 'index.html', // output file
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
     ],
-  },
-  optimization: {
-    minimizer: [
-      new CssMinimizerPlugin(),
-    ],
-  }
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            //JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ],
+    }
 };
